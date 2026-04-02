835363976b395adc5a78dcb8323e9206fe868d5e
AmeerHamza-max <ameerhamzarana0000786@gmail.com>
2026-04-02 13:09:59 +0500

    Operators Type Casting completed documentation remaining

---
+++ Lecture-2-Write-Your-First-Program-in-CPP/Operators/app.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a = 5;
+    cout<< a/20<<endl;
+
+    cout << a/20.0<< endl;
+
+    float f= 5.0;
+
+    cout<< f/20 <<endl;
+
+
+    int a = 2;
+    int b= 3;
+    bool first = (a==b);
+    cout<< first << endl;
+
+     bool second = (a>b);
+    cout<< second << endl;
+
+     bool third = (a<b);
+    cout<< third << endl;
+
+     bool fourth = (a>=b);
+    cout<< fourth << endl;
+
+     bool fifth = (a<=b);
+    cout<< fifth << endl;
+
+     bool sixth = (a!=b);
+    cout<< sixth << endl;
+
+    int l= 21;
+    cout << !l << endl;
+
+    
+
+    
+}
---
+++ Lecture-2-Write-Your-First-Program-in-CPP/Type Casting/app.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 'a';
+    cout<< a <<endl;//97
+
+    char ch = 98;
+    cout<< ch <<endl;//b
+
+    // char ch1 = 123456;
+    // cout<< ch1 <<endl;
+
+    unsigned int unsign =-122;
+    cout<< unsign <<endl;
+}
