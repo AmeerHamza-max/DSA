6653646d7897af4b4639e24055e33f4ade8b5377
AmeerHamza-max <ameerhamzarana0000786@gmail.com>
2026-04-23 22:35:35 +0500

    Pattern

---
+++ Lecture-4-Solving_Pattern-Questions/pattern1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i = 1; 
+    int n ; 
+    cout<<"Enter a Number = ";
+    cin >> n ;
+    while ( i <= n ){
+        int j = 1; 
+        while(j <= n ){
+            cout << j <<" ";
+            j++;
+        }
+        i++;
+        cout<<endl;
+
+    }
+}
